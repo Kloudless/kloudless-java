4838483fae639e3c0391de157e22fda09231082e
Guo-Guang Chiou <gchiou@kloudless.com>
2017-06-10 00:22:34 +0800

    Change slightly test cases based on be4bd0d

--- src/test/java/com/kloudless/KloudlessEventsTest.java
+++ src/test/java/com/kloudless/KloudlessEventsTest.java
@@ -4,8 +4,10 @@ import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.gson.Gson;
@@ -17,28 +19,23 @@ import com.kloudless.model.File;
 import com.kloudless.model.FileCollection;
 import com.kloudless.model.Folder;
 
+import static com.kloudless.StaticImporter.TestInfo;
+
 public class KloudlessEventsTest {
 
 	static Gson GSON = new GsonBuilder().create();
-	static ArrayList<String> accounts = new ArrayList<String>();
-	HashMap<String, Object> params = new HashMap<String, Object>();
-	HashMap<String, Object> metadata = new HashMap<String, Object>();
+	static List<String> accounts = new ArrayList<>();
+	HashMap<String, Object> params = new HashMap<>();
+	HashMap<String, Object> metadata = new HashMap<>();
 	
 	@BeforeClass
 	public static void setUp() {
-		Kloudless.apiKey = "INSERT API KEY HERE";
-
-		// Google Drive
-//		accounts.add("1");
-		// OneDrive
-//		accounts.add("2");
-		// Box
-//		accounts.add("3");
-		// Dropbox
-//		accounts.add("4");
+		Kloudless.apiKey = TestInfo.getApiKey();
+		accounts = TestInfo.getTestAccounts();
 	}
 
 	@Test
+  @Ignore
 	public void testCreateEvents() throws KloudlessException, FileNotFoundException, UnsupportedEncodingException, InterruptedException {
 		for (String account : accounts) {
 
@@ -116,7 +113,9 @@ public class KloudlessEventsTest {
 	public void testAccountRecent() throws KloudlessException {
 		for (String account : accounts) {
 			FileCollection results = Account.recent(account, null);
-			System.out.println(results);
+			if(results.count > 0) {
+				//TODO: assert returned objects
+			}
 		}
 	}
 
@@ -124,14 +123,10 @@ public class KloudlessEventsTest {
 	@Test
 	public void testAccountEvents() throws KloudlessException {
 		for (String account : accounts) {
-			params.clear();
-//			params.put("cursor", 11179); // gdrive
-//			params.put("cursor", 11715); // onedrive
-//			params.put("cursor", 11196); // box
-//			params.put("cursor", 11666); // dropbox
-			
 			EventCollection events = Account.events(account, params);
-			System.out.println(events);
+			if(events.count > 0) {
+				//TODO: assert returned objects
+			}
 		}
 	}
 }
